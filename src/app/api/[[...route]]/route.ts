cdffc31fde29c1e21464f3c606728310d3c39987
Chinmay Mhatre <chinmaymhatre111@gmail.com>
2024-05-10 23:05:35 +0530

    added hono get all route

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,11 @@
+import { Hono } from "hono"
+
+export const runtime = "edge";
+
+const app = new Hono().basePath('/api');
+
+app.get('/search', (c) => {
+  return c.json({});
+})
+
+export const GET = handle(app);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [input, setInput] = useState<string>('');
+  const [searchResults, setSearchResults] = useState<{
+    results: string[]
+    duration: number
+  }>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!input) return setSearchResults(undefined);
+
+      const res = await fetch(`/api/search?q=${input}`);
+    }
+  }, [input])
+
   return (
-    <>
-      <h1>Meow Nvim</h1>
-    </>
+    <div>
+      <input
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value)
+        }}
+        type="text"
+      />
+    </div
+    >
   );
 }
